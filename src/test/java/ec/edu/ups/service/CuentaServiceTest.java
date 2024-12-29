7106e8bc27c27261ec9566e0bf24e73e807ca0f5
AdrianC47 <luiscabreraucme@hotmail.com>
2024-12-29 15:30:45 -0500

    Test: testTransferenciaMontoNegativo Desc: Prueba para validar transferencia fallida con fondos negativos Return: Transacción fallida con mmontos negativos Phase: failed Result: Prueba fallida

--- src/test/java/ec/edu/ups/service/CuentaServiceTest.java
+++ src/test/java/ec/edu/ups/service/CuentaServiceTest.java
@@ -73,5 +73,23 @@ public class CuentaServiceTest {
         assertEquals(500.0, destino.getSaldo(), "El saldo de destino no debe cambiar");
     }
 
+    @Test
+    public void testTransferenciaMontoNegativo() {
+        // Configurar datos de prueba
+        Cuenta origen = new Cuenta(1L, null, 1001, 1000.0);
+        Cuenta destino = new Cuenta(2L, null, 1002, 500.0);
+
+        when(cuentaRepository.findByNumeroCuenta(1001)).thenReturn(origen);
+        when(cuentaRepository.findByNumeroCuenta(1002)).thenReturn(destino);
+
+        // Llamar al metodo
+        Transaccion transaccion = cuentaService.transferirDinero(origen, destino, -100.0, "Monto negativo");
+
+        // Validaciones
+        assertNotNull(transaccion, "La transacción no debe ser nula");
+        assertFalse(transaccion.getExito(), "La transacción debe fallar");
+        assertEquals(1000.0, origen.getSaldo(), "El saldo de origen no debe cambiar");
+        assertEquals(500.0, destino.getSaldo(), "El saldo de destino no debe cambiar");
+    }
 
 }
